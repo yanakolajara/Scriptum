98c17458b48908e0be4e9858ece429e04d179f98
yanakolajara <yanakolajara@pursuit.org>
2025-01-14 01:35:04 -0500

    style: basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import Chat from './components/chat/Chat';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/Home/Home';
+import './App.scss';
 
 function App() {
   return (
     <div className='App'>
-      <Chat />
+      <Navbar />
+      <div className='content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/chat' element={<Chat />} />
+          <Route path='*' element={<h1>Not found</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
