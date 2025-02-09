9756447ea8667156c85fe2a7fc3addff61baabf4
yanakolajara <yanakolajara@pursuit.org>
2025-02-09 05:34:29 -0500

    App name change

--- client/src/components/layout/Navbar/Header.jsx
+++ client/src/components/layout/Navbar/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header className='navbar__header bg-secondary'>
-      <h2 className='app-logo'>AI-JOURNAL</h2>
+      <h2 className='app-logo'>Scriptum</h2>
     </header>
   );
 }
