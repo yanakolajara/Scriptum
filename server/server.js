57ba8632b33f4737f28baaa5e44bc36aaef0ae0b
yanakolajara <yanakolajara@pursuit.org>
2025-02-09 05:45:35 -0500

    port fix

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ import { createApp } from './app.js';
 import { UserModel } from './models/postgresql/user.js';
 
 const app = createApp({ userModel: UserModel });
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 0;
 
 export const server = app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
+  const port = server.address().port;
+  console.log(`Server is running on port ${port}`);
 });
