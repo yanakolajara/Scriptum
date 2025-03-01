7ffc5764c2cc95b6eeb5db7f27949f8ee8961333
yanakolajara <yanakolajara@pursuit.org>
2025-03-01 15:23:15 -0500

    feat(chat): ai stream messages are directly added to the conversation, chunk by chunk

--- client/src/pages/Chat/components/ChatDisplay.jsx
+++ client/src/pages/Chat/components/ChatDisplay.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import ChatMessage from './ChatMessage';
 
-export default function ChatDisplay({ chatArr, streaming, loading }) {
+export default function ChatDisplay({ chat, loading }) {
   return (
     <div className='chat-container'>
-      {chatArr.map((msg, index) => (
+      {chat.map((msg, index) => (
         <ChatMessage key={index} msg={msg} />
       ))}
-      {loading && (
-        <p>
-          <strong>IA:</strong> {streaming}{' '}
-          <span className='blinking-cursor'>|</span>
-        </p>
-      )}
     </div>
   );
 }
