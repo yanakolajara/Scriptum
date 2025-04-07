25d80714f48bf6664c056a1a69b70f327c13812b
yanakolajara <yanakolajara@pursuit.org>
2025-04-07 13:56:30 -0400

    Add fallback localhost URL if VITE_API_URL env variable is not set

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_KEY,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
   timeout: 5000,
 });
