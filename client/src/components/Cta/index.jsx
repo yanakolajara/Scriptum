41bfe6fe71394c02fbf62a9d9f72a819c3a51173
yanakolajara <yanakolajara@pursuit.org>
2025-04-12 15:27:40 -0400

    feat(Cta): implemented a dynamic text rendering based on the props received

--- client/src/components/Cta/index.jsx
+++ client/src/components/Cta/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export const Cta = ({ text, onClick, style, ...props }) => {
+export const Cta = ({ text, children, onClick, style, ...props }) => {
   const className = `border-1 rounded-md p-1.5 hover:bg-gray-200 cursor-pointer ${style}`;
   return (
     <button className={className} onClick={onClick} {...props}>
-      {text}
+      {children || text || 'CTA'}
     </button>
   );
 };
