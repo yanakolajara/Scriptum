5a2ead1e44071bcf67a66efb17e56674111186ee
yanakolajara <yanakolajara@pursuit.org>
2025-04-21 20:28:01 -0400

    Update CORS origin URL to new client domain scriptum-app.vercel.app

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ export const createApp = ({ userModel, entryModel }) => {
     credentials: true,
     origin:
       config.app.environment === 'production'
-        ? 'https://scriptum-client.vercel.app/'
+        ? 'https://scriptum-app.vercel.app/'
         : 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add other methods if needed
     allowedHeaders: ['Content-Type'], // Add other headers if needed
